390e7d092722efaba23da7f436a58f8220faac07
Never <Never1750@163.com>
2015-11-01 11:02:39 +0800

    新建按钮

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -18,15 +18,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor grayColor];
-    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(30, 30, 100, 30)];
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(30, 30, 200, 30)];
     lab.text = @"这是一个测试Label";
     lab.backgroundColor = [UIColor greenColor];
     [self.view addSubview:lab];
 
     NSLog(@"test2");
 
-  NSLog(@"test");
-
+    NSLog(@"test");
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(30, 60, 200, 50);
+    [btn setTitle:@"新建一个按钮" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:btn];
 }
 
 - (void)didReceiveMemoryWarning {
